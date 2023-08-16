c2cfed59371f74f3b90cd8bfa31672d9cbedb171
Fritzie Primananda Adi Praja <primanandafritzie@gmail.com>
2023-08-17 00:44:49 +0800

    try using video route pt 2

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 // const bodyParser = require('body-parser');
 const connectDB = require('./src/utils/db');
 // const authRoutes = require('./src/routes/authRoutes');
-const videoRoutes = require('./src/routes/videoRoutes');
+// const videoRoutes = require('./src/routes/videoRoutes');
+const videoController = require('./controllers/videoController');
 // const productRoutes = require('./src/routes/productRoutes');
 // const commentRoutes = require('./src/routes/commentRoutes');
 
@@ -20,8 +21,10 @@ const videoRoutes = require('./src/routes/videoRoutes');
 // // Connect to the database
 connectDB();
 
+router.get('/videos', videoController.getVideos);
+
 // // Routes
-app.use('/api/video', videoRoutes);
+// app.use('/api/video', videoRoutes);
 // app.use('/api/product', productRoutes);
 // app.use('/api/comment', commentRoutes);
 
