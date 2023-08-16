67766151ea8c3062556a0c743d87a08a01f29282
Fritzie Primananda Adi Praja <primanandafritzie@gmail.com>
2023-08-17 00:37:31 +0800

    trial and  error

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
-// index.js
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const connectDB = require('./src/utils/db');
-const authRoutes = require('./src/routes/authRoutes');
-const videoRoutes = require('./src/routes/videoRoutes');
-const productRoutes = require('./src/routes/productRoutes');
-const commentRoutes = require('./src/routes/commentRoutes');
+// // index.js
+// const express = require('express');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+// const connectDB = require('./src/utils/db');
+// const authRoutes = require('./src/routes/authRoutes');
+// const videoRoutes = require('./src/routes/videoRoutes');
+// const productRoutes = require('./src/routes/productRoutes');
+// const commentRoutes = require('./src/routes/commentRoutes');
 
-const app = express();
+// const app = express();
 
-require('dotenv').config();
+// require('dotenv').config();
 
-// Middleware
-// Allow cross-origin requests from any origin
-app.use(cors());
-app.use(bodyParser.json());
+// // Middleware
+// // Allow cross-origin requests from any origin
+// app.use(cors());
+// app.use(bodyParser.json());
 
-// Connect to the database
-connectDB();
+// // Connect to the database
+// connectDB();
 
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/video', videoRoutes);
-app.use('/api/product', productRoutes);
-app.use('/api/comment', commentRoutes);
+// // Routes
+// app.use('/api/auth', authRoutes);
+// app.use('/api/video', videoRoutes);
+// app.use('/api/product', productRoutes);
+// app.use('/api/comment', commentRoutes);
 
+// app.all('/', (req, res) => {
+//   console.log("Just got a request!")
+//   res.send('Yo!')
+// })
+
+// // Start the server
+// app.listen(process.env.PORT || 3000)
+
+
+const express = require('express')
+const app = express()
 app.all('/', (req, res) => {
-  console.log("Just got a request!")
-  res.send('Yo!')
+    console.log("Just got a request!")
+    res.send('Yo!')
 })
-
-// Start the server
 app.listen(process.env.PORT || 3000)
