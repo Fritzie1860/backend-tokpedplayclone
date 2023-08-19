e20118a123b72c64876e7e90a950d9e56066ed1d
Fritzie Primananda Adi Praja <primanandafritzie@gmail.com>
2023-08-19 09:58:41 +0800

    solving cors

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // // index.js
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 // const bodyParser = require('body-parser');
-const connectDB = require('./src/utils/db');
+const connectDB = require("./src/utils/db");
 // const authRoutes = require('./src/routes/authRoutes');
 // const videoRoutes = require('./src/routes/videoRoutes');
-const videoController = require('./src/controllers/videoController');
-const productController = require('./src/controllers/productController');
-const commentController = require('./src/controllers/commentController');
+const videoController = require("./src/controllers/videoController");
+const productController = require("./src/controllers/productController");
+const commentController = require("./src/controllers/commentController");
 
 // const productRoutes = require('./src/routes/productRoutes');
 // const commentRoutes = require('./src/routes/commentRoutes');
@@ -18,11 +18,13 @@ const app = express();
 
 // // Middleware
 // // Allow cross-origin requests from any origin
-app.use(cors({
-    origin: 'https://tokpedplay-fritzie.000webhostapp.com',
-    methods: ['GET', 'POST'],
-    allowedHeaders: '*'
-  }));
+const corsOptions = {
+  origin: "https://try.com", // Replace with your allowed origin(s)
+  optionsSuccessStatus: 200,
+  allowedHeaders: "*",
+};
+
+app.use(cors(corsOptions));
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', 'https://tokpedplay-fritzie.000webhostapp.com'); // Tambahkan domain ini
 //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -33,10 +35,10 @@ app.use(cors({
 // // Connect to the database
 connectDB();
 
-app.get('/videos', videoController.getVideos);
-app.get('/products/:video_id', productController.getProductsByVideo);
-app.get('/comments/:video_id', commentController.getCommentsByVideo);
-app.post('/comments', commentController.submitComment);
+app.get("/videos", videoController.getVideos);
+app.get("/products/:video_id", productController.getProductsByVideo);
+app.get("/comments/:video_id", commentController.getCommentsByVideo);
+app.post("/comments", commentController.submitComment);
 
 // // Routes
 // app.use('/api/video', videoRoutes);
@@ -51,11 +53,10 @@ app.post('/comments', commentController.submitComment);
 // // Start the server
 // app.listen(process.env.PORT || 3000)
 
-
 // const express = require('express')
 // const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+app.all("/", (req, res) => {
+  console.log("Just got a request!");
+  res.send("Yo!");
+});
+app.listen(process.env.PORT || 3000);
