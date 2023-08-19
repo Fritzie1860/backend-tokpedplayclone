ab2ae94f0d2612bf6050e46a7ae1dc599123141d
Fritzie Primananda Adi Praja <primanandafritzie@gmail.com>
2023-08-19 22:36:44 +0800

    change to render + adding body-parser

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
-// // index.js
 const express = require("express");
 const cors = require("cors");
-// const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const connectDB = require("./src/utils/db");
-// const authRoutes = require('./src/routes/authRoutes');
-// const videoRoutes = require('./src/routes/videoRoutes');
 const videoController = require("./src/controllers/videoController");
 const productController = require("./src/controllers/productController");
 const commentController = require("./src/controllers/commentController");
 
-// const productRoutes = require('./src/routes/productRoutes');
-// const commentRoutes = require('./src/routes/commentRoutes');
-
 const app = express();
-
-// require('dotenv').config();
-
-// // Middleware
-// // Allow cross-origin requests from any origin
-// app.use(cors());
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'https://tokpedplay-fritzie.000webhostapp.com'); // Tambahkan domain ini
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     res.setHeader('Access-Control-Allow-Credentials', 'true');
-//     next();
-// });
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -33,14 +16,14 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   if ("OPTIONS" == req.method) {
     res.sendStatus(200);
   } else {
     next();
   }
 });
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 // // Connect to the database
 connectDB();
@@ -50,21 +33,6 @@ app.get("/products/:video_id", productController.getProductsByVideo);
 app.get("/comments/:video_id", commentController.getCommentsByVideo);
 app.post("/comments", commentController.submitComment);
 
-// // Routes
-// app.use('/api/video', videoRoutes);
-// app.use('/api/product', productRoutes);
-// app.use('/api/comment', commentRoutes);
-
-// app.all('/', (req, res) => {
-//   console.log("Just got a request!")
-//   res.send('Yo!')
-// })
-
-// // Start the server
-// app.listen(process.env.PORT || 3001)
-
-// const express = require('express')
-// const app = express()
 app.all("/", (req, res) => {
   console.log("Just got a request!");
   res.send("Yo!");
