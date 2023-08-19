447d1f3a541f57e98fee7e88e77fda20ccad04fd
Fritzie Primananda Adi Praja <primanandafritzie@gmail.com>
2023-08-19 10:17:08 +0800

    adding http status code

--- index.js
+++ index.js
@@ -3,14 +3,9 @@ const express = require("express");
 const cors = require("cors");
 // const bodyParser = require('body-parser');
 const connectDB = require("./src/utils/db");
-// const authRoutes = require('./src/routes/authRoutes');
-// const videoRoutes = require('./src/routes/videoRoutes');
 const videoController = require("./src/controllers/videoController");
 const productController = require("./src/controllers/productController");
 const commentController = require("./src/controllers/commentController");
-
-// const productRoutes = require('./src/routes/productRoutes');
-// const commentRoutes = require('./src/routes/commentRoutes');
 
 const app = express();
 
@@ -25,12 +20,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'https://tokpedplay-fritzie.000webhostapp.com'); // Tambahkan domain ini
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     next();
-// });
-// app.use(bodyParser.json());
 
 // // Connect to the database
 connectDB();
@@ -40,21 +29,6 @@ app.get("/products/:video_id", productController.getProductsByVideo);
 app.get("/comments/:video_id", commentController.getCommentsByVideo);
 app.post("/comments", commentController.submitComment);
 
-// // Routes
-// app.use('/api/video', videoRoutes);
-// app.use('/api/product', productRoutes);
-// app.use('/api/comment', commentRoutes);
-
-// app.all('/', (req, res) => {
-//   console.log("Just got a request!")
-//   res.send('Yo!')
-// })
-
-// // Start the server
-// app.listen(process.env.PORT || 3000)
-
-// const express = require('express')
-// const app = express()
 app.all("/", (req, res) => {
   console.log("Just got a request!");
   res.send("Yo!");
--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -1,14 +1,19 @@
 // src/controllers/commentController.js
-const Comment = require('../models/Comment');
+const Comment = require("../models/Comment");
 
 // Get a list of comments for a specific video
 const getCommentsByVideo = async (req, res) => {
   try {
     const video_id = req.params.video_id;
-    const comments = await Comment.find({ video_id }, '_id username comment timestamp').sort({ timestamp: -1 }).exec();
+    const comments = await Comment.find(
+      { video_id },
+      "_id username comment timestamp"
+    )
+      .sort({ timestamp: -1 })
+      .exec();
     res.json(comments);
   } catch (err) {
-    res.status(500).json({ error: 'Unable to fetch comments.' });
+    res.status(500).json({ error: "Unable to fetch comments." });
   }
 };
 
@@ -17,18 +22,25 @@ const submitComment = async (req, res) => {
   try {
     console.log(JSON.stringify(req.body));
     const { username, comment, video_id } = req.body;
-    const videosData = [{
-      video_id : video_id,
-      comment : comment,
-      username : username
-    }];
+    const videosData = [
+      {
+        video_id: video_id,
+        comment: comment,
+        username: username,
+      },
+    ];
     // const newComment = new Comment({ username, comment, video_id });
     // await newComment.save();
     // await Comment.insertMany(videosData);
     await comment.create(videosData);
-    res.json({ success: true, message: 'Comment submitted successfully.' , data: videosData });
+    res.json({
+      success: true,
+      message: "Comment submitted successfully.",
+      data: videosData,
+    });
   } catch (err) {
-    res.status(500).json({ error: 'Unable to submit comment.' , data: err});
+    console.error("Error submitting comment:", err);
+    res.status(500).json({ error: "Unable to submit comment.", data: err });
   }
 };
 
