d2966d481477e3c5709e7073531136a04737c240
Fritzie Primananda Adi Praja <primanandafritzie@gmail.com>
2023-08-19 10:27:54 +0800

    solve Missing Header problem

--- index.js
+++ index.js
@@ -13,11 +13,20 @@ const app = express();
 
 // // Middleware
 // // Allow cross-origin requests from any origin
+
 const corsOptions = {
   origin: "https://tokpedplay-fritzie.000webhostapp.com", // Replace with your allowed origin(s)
   optionsSuccessStatus: 200,
   allowedHeaders: "*",
 };
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Permissions-Policy",
+    "ch-ua-full-version=(Chrome/88.0.4324.182);ch-ua-platform=Windows;ch-ua-platform-version=10.0.19041;ch-ua-model="
+  );
+  next();
+});
 
 app.use(cors(corsOptions));
 
