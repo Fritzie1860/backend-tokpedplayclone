4bd79ae3cd975a3696eaca2601401342ca66ac3f
Fritzie Primananda Adi Praja <primanandafritzie@gmail.com>
2023-08-19 21:43:38 +0800

    push to cyclic [Access-Control-Allow-Credentials]

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(cors());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://tokpedplay-fritzie.000webhostapp.com'); // Tambahkan domain ini
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
 });
 // app.use(bodyParser.json());
